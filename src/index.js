e7922ca40e27771d367846bec4e4070f7c755f54
Yoskutik <yoskutik@wisebits.com>
2023-05-07 17:19:28 +0300

    Example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'core-js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = React.lazy(() => import('./App'));
+
+const root = ReactDOM.createRoot(document.body);
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="Loading...">
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
